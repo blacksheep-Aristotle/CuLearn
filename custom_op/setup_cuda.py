62e0171e1b2a0a4b8c242d90472191e56e2a9012
blacksheep-Aristotle <zhangweilong01@baidu.com>
2025-10-16 15:00:25 +0800

    fix setup_cuda error

--- custom_op/setup_cuda.py
+++ custom_op/setup_cuda.py
@@ -1,9 +1,14 @@
 from paddle.utils.cpp_extension import CUDAExtension, setup
 
+extra_compile_args = {
+    'cxx': ['-O2', '-std=c++17'],  # C++编译选项
+    'nvcc': ['-O2', '-std=c++17']  # NVCC编译选项
+}
 setup(
     name='custom_setup_ops',
     ext_modules=CUDAExtension(
-        sources=['relu_cuda.cc', 'relu_cuda.cu']
+        sources=['relu_cuda.cc', 'relu_cuda.cu'],
+        extra_compile_args=extra_compile_args
     )
 )
 
